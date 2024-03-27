501bf578bc874fea2baefe819ddff493d3aed07d
alexkuehnl <kuehnla2@gmail.com>
2024-03-27 00:15:38 -0500

    Supports .data file generation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,34 +40,71 @@ public class Main {
   }
 
   private static void data(BufferedReader br, String path) throws IOException {
+
+    // TODO: needs to store addresses of data
+
     String outName = fileName(path) + ".data";
     path = basePath(path) + outName;
     BufferedWriter bw = new BufferedWriter(new FileWriter(path));
     StringBuilder sb = new StringBuilder();
+    int j = 0;
     while (br.ready()) {
       br.mark(1);
-      if (br.readLine().equals(".text")) {
-        br.reset();
-        return;
-      }
-      br.reset();
-      br.mark(1);
-      if (br.readLine().equals(".data")) {
-        br.reset();
-        return;
-      }
-      br.reset();
-
       String line = br.readLine();
+
+      if (line.equals(".text") || line.equals(".data")) {
+        br.reset();
+        break;
+      }
+
+      if (line.isEmpty()) continue;
+
       Pattern p = Pattern.compile("\"([^\"]*)\"");
       Matcher m = p.matcher(line);
       while (m.find()) {
         line = m.group(1);
       }
 
+      System.out.println(line);
 
+      // Integer.toHexString(Integer.parseInt(dec)
+      for (int i = 0; i < line.length(); ++i, ++j) {
+        if (j > 3) {
+          j = 0;
+          bw.write(sb.toString());
+          bw.newLine();
+          bw.flush();
+          System.out.println("WRITING: " + sb.toString());
+          sb.delete(0, sb.length());
+        }
 
+        sb.insert(0, Integer.toHexString(line.charAt(i)));
+      }
+
+      if (j > 3) {
+        j = 1;
+        bw.write(sb.toString());
+        bw.newLine();
+        bw.flush();
+        System.out.println("WRITING: " + sb.toString());
+        sb.delete(0, sb.length());
+        sb.insert(0, "00");
+      } else {
+        sb.insert(0, "00");
+        ++j;
+      }
     }
+
+    if (!sb.isEmpty()) {
+      while (sb.length() < 8)
+        sb.insert(0, 0);
+
+      bw.write(sb.toString());
+      bw.newLine();
+      bw.flush();
+    }
+    bw.close();
+
   }
 
   private static String basePath(String path) {
