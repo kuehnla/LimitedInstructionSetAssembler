6c3c91541a1a9f10cfb21c005d968d954b9ce406
alexkuehnl <kuehnla2@gmail.com>
2024-03-26 21:06:23 -0500

    Adding functionality to produce .data and .text files from .asm file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import java.io.*;
+
 public class Main {
   public static void main(String[] args) {
-    String[] argz = args[0].split(" ");
-    Instruction in = initOp(argz);
-    in.toMachine(argz);
-    System.out.print(in.getWord() + "\n");
+    if (!args[0].contains("asm")) {
+      String[] argz = args[0].split(" ");
+      Instruction in = singleInstruction(argz);
+      in.toMachine(argz);
+      System.out.print(in.getWord() + "\n");
+    } else {
+      try {
+        assemblyFile(args[0]);
+      } catch (IOException e) {
+        System.out.println(e.getMessage());
+      }
+    }
   }
 
-  private static AbstractInstruction initOp(String[] argz) {
+  private static AbstractInstruction singleInstruction(String[] argz) {
     String op = null;
     for (String s : argz) {
       if (s == null || s.isEmpty()) continue;
@@ -17,6 +27,15 @@ public class Main {
     }
     assert op != null;
     op = op.split("#")[0];
+    return initOp(op);
+  }
+
+  private static void assemblyFile(String path) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader(path));
+    System.out.println(br.readLine());
+  }
+
+  private static AbstractInstruction initOp(String op) {
     return switch (op) {
       // R-TYPES:
       case "add" -> new RegisterType("100000");
@@ -38,6 +57,7 @@ public class Main {
       // SYSCALL:
       case "syscall" -> new Syscall();
       default -> null;
+      // TODO : li la blt
     };
   }
 }
